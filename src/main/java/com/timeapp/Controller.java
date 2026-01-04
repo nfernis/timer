1a51b5fef26e27e59f80b4b600ec9b9dcf1a0d96
Анастасия <af7935053@gmail.com>
2026-01-04 17:28:14 +0300

    доделала маску для ввода времени inputTime

--- src/main/java/com/timeapp/Controller.java
+++ src/main/java/com/timeapp/Controller.java
@@ -26,19 +26,47 @@ public class Controller {
     @FXML
     private void initialize() {
         inputTime(timeComeToWork);
+        inputTime(timeGoToLunch);
+        inputTime(timeComebackFromLunch);
+        inputTime(timeQuitMyJob);
     }
+
     //Маска для ввода времени
-    private void inputTime(TextField field){
+    private void inputTime(TextField field) {
         field.setTextFormatter(new TextFormatter<>(change -> {
-            //TODO
-
+            if (change.isDeleted()) return change;
             return change;
-
-
         }));
-        field.textProperty().addListener((obs, oldVal, newVal) -> {
-            if (newVal.length() > 5) {
-                field.setText(oldVal);
+
+        field.textProperty().addListener((obs, oldValue, newValue) -> {
+            if (newValue == null) return;
+            //проверка на числа
+            if (!newValue.matches("^\\d{0,2}:?\\d{0,2}$")) {
+                field.setText(oldValue);
+                return;
+            }
+            //проверка длины
+            if (newValue.length() > 5) {
+                field.setText(oldValue);
+                return;
+            }
+            //не больше 23 часов
+            if (oldValue.length() == 0 && Integer.parseInt(newValue) > 2){
+                field.setText(oldValue);
+                return;
+            }
+            //не больше 59 минут
+            if (oldValue.length() == 3 && Integer.parseInt(newValue.substring(newValue.length()-1)) > 5){
+                field.setText(oldValue);
+                return;
+            }
+            // автодобавление ":"
+            if (oldValue != null && oldValue.length() < newValue.length() &&
+                    newValue.length() == 2 && newValue.matches("\\d{2}")) {
+                javafx.application.Platform.runLater(() -> {
+                    field.setText(newValue + ":");
+                    field.positionCaret(3);
+                });
             }
         });
     }
